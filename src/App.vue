8d9cfd162cee817f5725f415e85c757dac81e46c
Ben Montgomery <ben@modernthings.net>
2025-06-13 13:24:42 -0600

    New web portfolio project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="mt-10 mb-10 bg-gray-500 text-center text-4xl">Welcome to my Website</h1>
+      <nav class="text-2xl">
+        <RouterLink
+          class="[&.router-link-exact-active]:text-red-500 [&.router-link-exact-active]:hover:text-purple-400"
+          to="/"
+          >Home</RouterLink
+        >
+        <RouterLink
+          class="[&.router-link-exact-active]:text-red-500 [&.router-link-exact-active]:hover:text-purple-400"
+          to="/about"
+          >About</RouterLink
+        >
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="bg-gray-500 text-4xl">The Welcome Was here</h1>
+  </main>
+</template>
