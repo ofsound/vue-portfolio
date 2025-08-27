500ad15798f1389fd37a371799faa6d740a125fd
Ben Montgomery <ben@modernthings.net>
2025-08-27 14:00:03 -0600

    Rough in playlist playing

--- src/components/PersistentPlayer.vue
+++ src/components/PersistentPlayer.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 let startTime = 0
+const trackIndex = ref(0)
 const elapsed = ref(0)
 const progressPercentage = ref(0)
+
+let firstPlay = true
 
 const audioContext = new window.AudioContext()
 let source = audioContext.createBufferSource()
@@ -15,22 +18,60 @@ let audioBuffer: AudioBuffer = new AudioBuffer({
   sampleRate: audioContext.sampleRate,
 })
 
-async function playMp3WithWebAudio(url: string): Promise<void> {
-  const response = await fetch(url)
-  const arrayBuffer = await response.arrayBuffer()
-  audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
-
-  source.buffer = audioBuffer
-
-  source.connect(gainNode).connect(audioContext.destination)
-
-  source.start(0)
+async function loadAudioBuffers(fileNames: Array<string>) {
+  const buffers = await Promise.all(
+    fileNames.map(async (file) => {
+      try {
+        const response = await fetch(`${file}`)
+        const arrayBuffer = await response.arrayBuffer()
+        return audioContext.decodeAudioData(arrayBuffer)
+      } catch (error) {
+        console.error(`Error loading or decoding ${file}:`, error)
+        return null
+      }
+    }),
+  )
+  return buffers.filter((buffer) => buffer !== null) // Filter out any failed loads
 }
 
-const playAudio = () => {
-  playMp3WithWebAudio('01.mp3').catch((error) => console.error('Error playing audio:', error))
+const audioData = [
+  { title: 'Ghost Camels', file: '01.mp3' },
+  { title: 'Abu Rawash', file: '02.mp3' },
+  { title: 'Get Thee Behind Me', file: '03.mp3' },
+]
+
+const buffers = await loadAudioBuffers(audioData.map((track) => track.file))
+
+const playAudio = (index: number) => {
+  if (source && !firstPlay) {
+    source.stop()
+  }
+
+  firstPlay = false
+
+  source = audioContext.createBufferSource()
+  if (buffers[index]) {
+    source.buffer = buffers[index]
+    audioBuffer = buffers[index]
+  }
+
+  source.connect(gainNode).connect(audioContext.destination)
+  source.start(0)
+
   startTime = audioContext.currentTime
   requestAnimationFrame(updateProgressBar)
+}
+
+const toggleAudio = () => {
+  if (audioContext.state === 'suspended') {
+    audioContext.resume()
+  } else {
+    audioContext.suspend()
+  }
+}
+
+const playlistClick = (index: number) => {
+  playAudio(index)
 }
 
 const seekAudio = (event: MouseEvent) => {
@@ -50,19 +91,9 @@ const seekAudio = (event: MouseEvent) => {
   startTime = audioContext.currentTime - startOffset
 }
 
-const toggleAudio = () => {
-  if (audioContext.state === 'suspended') {
-    audioContext.resume()
-  } else {
-    audioContext.suspend()
-  }
-}
-
 function updateProgressBar() {
   elapsed.value = audioContext.currentTime - startTime
-  if (audioBuffer) {
-    progressPercentage.value = (elapsed.value / audioBuffer.duration) * 100
-  }
+  progressPercentage.value = (elapsed.value / audioBuffer.duration) * 100
   requestAnimationFrame(updateProgressBar)
 }
 
@@ -71,6 +102,15 @@ function formatTime(seconds: number) {
   const remainingSeconds = Math.floor(seconds % 60)
   return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`
 }
+
+const currentTrackTitle = computed(() => {
+  const thisObject = audioData[trackIndex.value]
+  if (thisObject) {
+    return thisObject.title
+  } else {
+    return 'none'
+  }
+})
 
 onMounted(() => {
   const volumeControl = document.querySelector('#volume') as HTMLInputElement
@@ -87,27 +127,39 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="ml-auto flex h-12 w-full shrink-0 items-center justify-end gap-4 px-2">
-    <div class="h-full w-full bg-gray-200" @click="seekAudio">
-      <div
-        :style="{ width: progressPercentage + '%' }"
-        class="pointer-events-none h-full bg-green-800"
-      ></div>
+  <section class="ml-auto w-full shrink-0">
+    <div>
+      <a
+        v-for="(track, index) in audioData.map((track) => track.title)"
+        :key="track"
+        @click="playlistClick(index)"
+      >
+        {{ track }}
+      </a>
     </div>
-    <div>{{ formatTime(elapsed) }}</div>
-    <div>{{ formatTime(audioBuffer.duration) }}</div>
-    <input type="range" id="volume" min="0" max="2" value="1" step="0.01" />
-    <button
-      class="cursor-pointer rounded-xl bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
-      @click="playAudio"
-    >
-      play music ▸
-    </button>
-    <button
-      class="cursor-pointer rounded-xl bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
-      @click="toggleAudio"
-    >
-      toggle music ▸
-    </button>
+    <div>{{ currentTrackTitle }}</div>
+    <div class="flex h-12 items-center justify-end gap-4 px-2">
+      <div class="h-full w-full bg-gray-200" @click="seekAudio">
+        <div
+          :style="{ width: progressPercentage + '%' }"
+          class="pointer-events-none h-full bg-green-800"
+        ></div>
+      </div>
+      <div>{{ formatTime(elapsed) }}</div>
+      <div>{{ formatTime(audioBuffer.duration) }}</div>
+      <input type="range" id="volume" min="0" max="2" value="1" step="0.01" />
+      <!-- <button
+        class="cursor-pointer rounded-xl bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
+        @click="playAudio"
+      >
+        play
+      </button> -->
+      <button
+        class="cursor-pointer rounded-xl bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
+        @click="toggleAudio"
+      >
+        toggle
+      </button>
+    </div>
   </section>
 </template>
