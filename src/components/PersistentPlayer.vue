6b439b8a9379875510c6e2c2821a36743d58c731
Ben Montgomery <ben@modernthings.net>
2025-08-27 10:09:08 -0600

    Add rough volume/pause to player

--- src/components/PersistentPlayer.vue
+++ src/components/PersistentPlayer.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-async function playMp3WithWebAudio(url: string): Promise<void> {
-  const audioContext = new window.AudioContext()
+import { onMounted } from 'vue'
 
+const audioContext = new window.AudioContext()
+const gainNode = audioContext.createGain()
+
+async function playMp3WithWebAudio(url: string): Promise<void> {
   const response = await fetch(url)
   const arrayBuffer = await response.arrayBuffer()
   const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
@@ -9,7 +12,7 @@ async function playMp3WithWebAudio(url: string): Promise<void> {
   const source = audioContext.createBufferSource()
   source.buffer = audioBuffer
 
-  source.connect(audioContext.destination)
+  source.connect(gainNode).connect(audioContext.destination)
 
   source.start(0)
 }
@@ -17,15 +20,44 @@ async function playMp3WithWebAudio(url: string): Promise<void> {
 const playAudio = () => {
   playMp3WithWebAudio('01.mp3').catch((error) => console.error('Error playing audio:', error))
 }
+
+const toggleAudio = () => {
+  // Check if context is in suspended state (autoplay policy)
+  if (audioContext.state === 'suspended') {
+    audioContext.resume()
+  } else {
+    audioContext.suspend()
+  }
+}
+
+onMounted(() => {
+  const volumeControl = document.querySelector('#volume') as HTMLInputElement
+  if (volumeControl) {
+    volumeControl.addEventListener(
+      'input',
+      () => {
+        gainNode.gain.value = parseFloat(volumeControl.value)
+      },
+      false,
+    )
+  }
+})
 </script>
 
 <template>
   <section class="ml-auto flex h-12 max-w-max shrink-0 items-center justify-end px-2">
+    <input type="range" id="volume" min="0" max="2" value="1" step="0.01" />
     <button
       class="cursor-pointer rounded-xl bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
       @click="playAudio"
     >
       play music ▸
+    </button>
+    <button
+      class="cursor-pointer rounded-xl bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
+      @click="toggleAudio"
+    >
+      toggle music ▸
     </button>
   </section>
 </template>
