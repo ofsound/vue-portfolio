2ed2bb7aa6365cf4eb67da41cea30e1792ca2bde
Ben Montgomery <ben@modernthings.net>
2025-08-28 11:53:04 -0600

    Refactor Playlist as Component

--- src/components/PersistentPlayer.vue
+++ src/components/PersistentPlayer.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
 
+import SinglePlaylist from '@/components/player/SinglePlaylist.vue'
+
 import SpectrumVisualizer from '@/components/player/SpectrumVisualizer.vue'
 import WaveformVisualizer from '@/components/player/WaveformVisualizer.vue'
 
 let startTime = 0
-const trackIndex = ref(0)
+const armedIndex = ref(0)
 const elapsed = ref(0)
 const progressPercentage = ref(0)
 
@@ -86,20 +88,20 @@ const openPlaylist = () => {
 }
 
 const playlistClick = (index: number) => {
-  trackIndex.value = index
+  armedIndex.value = index
   playAudio(index)
 }
 
 const prevTrack = () => {
-  if (trackIndex.value > 0) {
-    trackIndex.value--
-    playAudio(trackIndex.value)
+  if (armedIndex.value > 0) {
+    armedIndex.value--
+    playAudio(armedIndex.value)
   }
 }
 const nextTrack = () => {
-  if (trackIndex.value < audioData.length - 1) {
-    trackIndex.value++
-    playAudio(trackIndex.value)
+  if (armedIndex.value < audioData.length - 1) {
+    armedIndex.value++
+    playAudio(armedIndex.value)
   }
 }
 
@@ -133,7 +135,7 @@ function formatTime(seconds: number) {
 }
 
 const currentTrackTitle = computed(() => {
-  const thisObject = audioData[trackIndex.value]
+  const thisObject = audioData[armedIndex.value]
   if (thisObject) {
     return thisObject.title
   } else {
@@ -157,104 +159,94 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="mb-8">
-    <section class="w-full shrink-0">
-      <Teleport to="body" v-if="playlistVisible">
-        <div class="absolute z-0 flex h-full w-full flex-col bg-gray-100 p-30">
-          <a
-            href="#"
-            class="block border-b-1 border-gray-400 px-1 py-2 hover:bg-gray-200"
-            v-for="(track, index) in audioData.map((track) => track.title)"
-            :key="track"
-            @click="playlistClick(index)"
-          >
-            <span class="mr-1 text-xs font-bold">{{ index + 1 }}.</span> {{ track }}
-            {{ index == trackIndex ? '*' : '' }}
-          </a>
-        </div>
-      </Teleport>
-      <div class="relative z-10">
-        <div class="flex h-20 basis-1/3 items-center justify-between gap-4 [&>*]:flex-1">
-          <div class="flex h-full flex-col">
-            <div class="mt-5 text-xl font-bold">
-              <div class="border-0 border-b-2 border-gray-300 px-1 py-1">
-                {{ currentTrackTitle }}
-              </div>
+  <SinglePlaylist
+    :armedIndex
+    :titles="audioData.map((track) => track.title)"
+    v-if="playlistVisible"
+    @trackClicked="playlistClick"
+  />
+  <div class="w-full shrink-0">
+    <div class="relative z-10">
+      <div class="flex h-20 basis-1/3 items-center justify-between gap-4 [&>*]:flex-1">
+        <div class="flex h-full flex-col">
+          <div class="mt-5 text-xl font-bold">
+            <div class="border-0 border-b-2 border-gray-300 px-1 py-1">
+              {{ currentTrackTitle }}
             </div>
           </div>
-          <div class="flex h-2/3 justify-center gap-2">
-            <button
-              class="mt-1 aspect-square max-h-7/8 cursor-pointer rounded-full bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm font-black hover:bg-gray-400"
-              @click="prevTrack"
-            >
-              &#9664;
-            </button>
-            <button
-              class="relative block aspect-square cursor-pointer rounded-full bg-linear-to-t from-sky-600 to-indigo-300 px-3 hover:bg-gray-400"
-              @click="toggleAudio"
-            >
-              <svg
-                v-if="audioContext.state === 'suspended'"
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute top-[15px] right-0 left-1 mr-auto ml-auto h-6 w-6"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  d="M133 440a35.37 35.37 0 01-17.5-4.67c-12-6.8-19.46-20-19.46-34.33V111c0-14.37 7.46-27.53 19.46-34.33a35.13 35.13 0 0135.77.45l247.85 148.36a36 36 0 010 61l-247.89 148.4A35.5 35.5 0 01133 440z"
-                />
-              </svg>
-              <svg
-                v-if="audioContext.state !== 'suspended'"
-                xmlns="http://www.w3.org/2000/svg"
-                class="absolute top-[15.5px] right-0 left-0 mr-auto ml-auto h-6 w-6"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  d="M208 432h-48a16 16 0 01-16-16V96a16 16 0 0116-16h48a16 16 0 0116 16v320a16 16 0 01-16 16zM352 432h-48a16 16 0 01-16-16V96a16 16 0 0116-16h48a16 16 0 0116 16v320a16 16 0 01-16 16z"
-                />
-              </svg>
-            </button>
-            <button
-              class="mt-1 aspect-square max-h-7/8 cursor-pointer rounded-full bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
-              @click="nextTrack"
-            >
-              &#9654;
-            </button>
-          </div>
-          <div class="flex h-full flex-col items-end px-2 text-right">
-            <button
-              class="mt-1 aspect-square rotate-[211deg] cursor-pointer rounded-full px-3 py-1 text-sm hover:bg-gray-400"
-              @click="openPlaylist"
-            >
-              &#9664;
-            </button>
-            <input
-              class="mt-8 max-w-7/8 px-2"
-              type="range"
-              id="volume"
-              min="0"
-              max="1.2"
-              v-model="rangeValue"
-              step="0.01"
-            />
-          </div>
         </div>
-        <div class="text-xs font-bold tabular-nums">
-          {{ formatTime(elapsed) }} / {{ formatTime(audioBuffer.duration) }}
+        <div class="flex h-2/3 justify-center gap-2">
+          <button
+            class="mt-1 aspect-square max-h-7/8 cursor-pointer rounded-full bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm font-black hover:bg-gray-400"
+            @click="prevTrack"
+          >
+            &#9664;
+          </button>
+          <button
+            class="relative block aspect-square cursor-pointer rounded-full bg-linear-to-t from-sky-600 to-indigo-300 px-3 hover:bg-gray-400"
+            @click="toggleAudio"
+          >
+            <svg
+              v-if="audioContext.state === 'suspended'"
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute top-[15px] right-0 left-1 mr-auto ml-auto h-6 w-6"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M133 440a35.37 35.37 0 01-17.5-4.67c-12-6.8-19.46-20-19.46-34.33V111c0-14.37 7.46-27.53 19.46-34.33a35.13 35.13 0 0135.77.45l247.85 148.36a36 36 0 010 61l-247.89 148.4A35.5 35.5 0 01133 440z"
+              />
+            </svg>
+            <svg
+              v-if="audioContext.state !== 'suspended'"
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute top-[15.5px] right-0 left-0 mr-auto ml-auto h-6 w-6"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M208 432h-48a16 16 0 01-16-16V96a16 16 0 0116-16h48a16 16 0 0116 16v320a16 16 0 01-16 16zM352 432h-48a16 16 0 01-16-16V96a16 16 0 0116-16h48a16 16 0 0116 16v320a16 16 0 01-16 16z"
+              />
+            </svg>
+          </button>
+          <button
+            class="mt-1 aspect-square max-h-7/8 cursor-pointer rounded-full bg-linear-to-t from-sky-600 to-indigo-300 px-3 py-1 text-sm hover:bg-gray-400"
+            @click="nextTrack"
+          >
+            &#9654;
+          </button>
         </div>
-        <div class="mt-1 opacity-80 hover:opacity-100">
-          <div class="flex h-4 w-full bg-gray-100" @click="seekAudio">
-            <div
-              :style="{ width: progressPercentage + '%' }"
-              class="pointer-events-none h-full bg-green-800"
-            ></div>
-          </div>
+        <div class="flex h-full flex-col items-end px-2 text-right">
+          <button
+            class="mt-1 aspect-square rotate-[211deg] cursor-pointer rounded-full px-3 py-1 text-sm hover:bg-gray-400"
+            @click="openPlaylist"
+          >
+            &#9664;
+          </button>
+          <input
+            class="mt-8 max-w-7/8 px-2"
+            type="range"
+            id="volume"
+            min="0"
+            max="1.2"
+            v-model="rangeValue"
+            step="0.01"
+          />
         </div>
       </div>
-      <div class="mt-6 hidden gap-8 bg-gray-900 p-10">
-        <SpectrumVisualizer :analyser />
-        <WaveformVisualizer :analyser />
+      <div class="text-xs font-bold tabular-nums">
+        {{ formatTime(elapsed) }} / {{ formatTime(audioBuffer.duration) }}
       </div>
-    </section>
+      <div class="mt-1 opacity-80 hover:opacity-100">
+        <div class="flex h-4 w-full bg-gray-100" @click="seekAudio">
+          <div
+            :style="{ width: progressPercentage + '%' }"
+            class="pointer-events-none h-full bg-green-800"
+          ></div>
+        </div>
+      </div>
+    </div>
+    <div class="mt-6 hidden gap-8 bg-gray-900 p-10">
+      <SpectrumVisualizer :analyser />
+      <WaveformVisualizer :analyser />
+    </div>
   </div>
 </template>
---
+++ src/components/player/SinglePlaylist.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps<{
+  armedIndex: number
+  titles: string[]
+}>()
+
+const emit = defineEmits(['trackClicked'])
+
+const isArmed = (index: number): string => {
+  if (props.armedIndex === index) {
+    return ''
+  } else {
+    return 'bg-[repeating-linear-gradient(45deg,_#e1e1e1_0,_#e1e1e1_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed text-gray-400'
+  }
+}
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="absolute z-0 flex h-full w-full flex-col bg-gray-100 p-30">
+      <a
+        href="#"
+        class="block border-b-1 border-gray-400 px-1 py-2 hover:bg-gray-200"
+        v-for="(title, index) in titles"
+        :key="title"
+        :class="isArmed(index)"
+        @click="emit('trackClicked', index)"
+      >
+        <span class="mr-1 text-xs font-bold">{{ index + 1 }}.</span> {{ title }}
+      </a>
+    </div>
+  </Teleport>
+</template>
