49aeaef6fb3e07390d8801af02ee0ca2cf07b23b
Ben Montgomery <ben@modernthings.net>
2025-10-06 19:09:55 -0600

    Progress towards more variations and control

--- src/components/player/SpectrumVisualizer.vue
+++ src/components/player/SpectrumVisualizer.vue
@@ -6,8 +6,6 @@ import { gsap } from 'gsap'
 import { PhysicsPropsPlugin } from 'gsap/PhysicsPropsPlugin'
 import { Physics2DPlugin } from 'gsap/Physics2DPlugin'
 
-// import { getRandomIntIncBip } from '@/utils/MathUtils.ts'
-
 gsap.registerPlugin(PhysicsPropsPlugin, Physics2DPlugin)
 
 const props = defineProps<{
@@ -23,67 +21,49 @@ const canvas = useTemplateRef('spectrumCanvas')
 
 const canvasWidth = ref(600) // make this dynamic
 
-const binGroupTotal = 8
+const itemRefs = useTemplateRef('items')
 
-const binGroupSize = bufferLength / binGroupTotal
-
-const binsArray = ref<number[][]>([])
-
-const binTotalsDisplay = ref<number[]>([])
-binTotalsDisplay.value = new Array(8).fill(0)
-
-const binTriggers = ref<boolean[]>([])
-binTriggers.value = new Array(8).fill(false)
-
-const div0 = ref(null)
-const div1 = ref(null)
-const div2 = ref(null)
-const div3 = ref(null)
-const div4 = ref(null)
-const div5 = ref(null)
-const div6 = ref(null)
-const div7 = ref(null)
-
-const divArray = [div0, div1, div2, div3, div4, div5, div6, div7]
-
-for (let index = 0; index < binGroupTotal; index++) {
-  const element = [binGroupSize * index, binGroupSize * (index + 1)]
-  binsArray.value.push(element)
+interface BinObject {
+  low: number
+  high: number
+  threshold: number
 }
 
-binsArray.value = [
-  [0, 1],
-  [2, 6],
-  [7, 10],
-  [11, 15],
-  [16, 22],
-  [23, 32],
-  [33, 64],
-  [65, 128],
-]
+const binObjects = ref<BinObject[]>([
+  { low: 0, high: 1, threshold: 350 },
+  { low: 2, high: 5, threshold: 180 },
+  { low: 6, high: 10, threshold: 180 },
+  { low: 11, high: 20, threshold: 180 },
+])
 
-const binThresholds = [220, 400, 300, 300, 500, 400, 200, 10]
+const binTotalsDisplay = ref<number[]>([])
+binTotalsDisplay.value = new Array(binObjects.value.length).fill(0)
+
+const binTriggers = ref<boolean[]>([])
+binTriggers.value = new Array(binObjects.value.length).fill(false)
 
 const handleTrigger = (thisBin: number) => {
-  for (let index = 0; index < divArray.length; index++) {
-    const thisBinRef = divArray[thisBin]
+  if (itemRefs.value) {
+    for (let index = 0; index < itemRefs.value.length; index++) {
+      const thisBinRef = itemRefs.value[thisBin]
 
-    if (thisBinRef) {
-      const thisDiv = thisBinRef.value
+      if (thisBinRef) {
+        const thisDiv = thisBinRef
 
-      gsap.to(thisDiv, {
-        duration: 0.05,
-        x: '+=1px',
-      })
+        gsap.to(thisDiv, {
+          duration: 0.05,
+          x: '+=1px',
+        })
+      }
     }
   }
 }
 
 watch(binTotalsDisplay, (newValue, oldValue) => {
-  for (let index = 0; index < binThresholds.length; index++) {
+  for (let index = 0; index < binObjects.value.length; index++) {
     const prevValue = oldValue[index] as number
     const thisValue = newValue[index] as number
-    const thisThreshold = binThresholds[index] as number
+    const thisThreshold = binObjects.value[index]?.threshold as number
 
     if (thisValue > thisThreshold) {
       if (prevValue < thisThreshold) {
@@ -94,6 +74,8 @@ watch(binTotalsDisplay, (newValue, oldValue) => {
 })
 
 onMounted(() => {
+  console.log(itemRefs.value)
+
   if (canvas.value) {
     const canvasContext = canvas.value.getContext('2d')
 
@@ -114,7 +96,7 @@ onMounted(() => {
         let barHeight
         let x = 0
 
-        const binTotals: number[] = new Array(8).fill(0)
+        const binTotals: number[] = new Array(binObjects.value.length).fill(0)
 
         for (let i = 0; i < bufferLength; i++) {
           const thisDataArray = dataArray[i]
@@ -122,21 +104,13 @@ onMounted(() => {
           if (thisDataArray) {
             barHeight = thisDataArray / 2
 
-            for (let j = 0; j < binsArray.value.length; j++) {
-              if (binsArray.value[j]) {
-                const thisBin = binsArray.value[j]
+            for (let j = 0; j < binObjects.value.length; j++) {
+              if (binObjects.value[j]) {
+                const thisBin = binObjects.value[j]
 
                 if (thisBin) {
-                  const lowerLimit = thisBin[0]
-                  const upperLimit = thisBin[1]
-
-                  // console.log('lower:', lowerLimit)
-                  // console.log('upper:', upperLimit)
-
-                  if (lowerLimit != null && upperLimit) {
-                    if (i >= lowerLimit && i < upperLimit) {
-                      // console.log(j)
-
+                  if (thisBin.low != null && thisBin.high) {
+                    if (i >= thisBin.low && i <= thisBin.high) {
                       binTotals[j] = thisDataArray + (binTotals[j] as number)
                     }
                   }
@@ -170,13 +144,13 @@ onUnmounted(() => {
     <div class="font-bold">Metrics</div>
     <div class="flex">
       <div>
-        <div v-for="(value, index) in binsArray" :key="index">
-          {{ value }}
+        <div v-for="(value, index) in binObjects" :key="index">
+          {{ value.low }}-{{ value.high }}
         </div>
       </div>
       <div class="ml-4 w-12">
-        <div v-for="(value, index) in binThresholds" :key="index">
-          {{ value }}
+        <div v-for="(value, index) in binObjects" :key="index">
+          {{ value.threshold }}
         </div>
       </div>
       <div class="ml-4 w-12">
@@ -185,14 +159,7 @@ onUnmounted(() => {
         </div>
       </div>
       <div class="ml-4">
-        <div ref="div0">*</div>
-        <div ref="div1">*</div>
-        <div ref="div2">*</div>
-        <div ref="div3">*</div>
-        <div ref="div4">*</div>
-        <div ref="div5">*</div>
-        <div ref="div6">*</div>
-        <div ref="div7">*</div>
+        <div v-for="(value, index) in binTotalsDisplay" :key="index" ref="items">*</div>
       </div>
     </div>
   </div>
